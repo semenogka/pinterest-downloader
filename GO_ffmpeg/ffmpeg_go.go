3fa433cce9b77cae47d41a761e1ceb02ed644d92
semenogka <akuninsemen79@gmail.com>
2026-05-16 18:00:33 +0500

    ffmpeg refactoring

---
+++ GO_ffmpeg/ffmpeg_go.go
@@ -0,0 +1,36 @@
+package goffmpeg
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// convertTSToMP4 converts a TS file to MP4.
+func convertTSToMP4(inputFile, outputFile string) error {
+	cmd := exec.Command("ffmpeg", "-i", inputFile, outputFile)
+	cmd.Run()
+
+	os.Remove(inputFile)
+	return nil 
+}
+
+// MergeVideoAndAudio merges video and audio into one file.
+func MergeVideoAndAudio(videoFile, audioFile, outputFile string) error {
+	cmd := exec.Command("ffmpeg", "-i", videoFile, "-i", audioFile, outputFile)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("cmd.Run() error: %v", err)
+	}
+	
+	os.Remove(videoFile)
+	os.Remove(audioFile)
+
+	return nil 
+}
+
+// convertToMP3 converts an audio file to MP3 format.
+func convertToMP3(inputFile, outputFile string) error {
+	cmd := exec.Command("ffmpeg", "-i", inputFile, "-vn", "-acodec", "libmp3lame", "-b:a", "192k", outputFile)
+	return cmd.Run()
+}
