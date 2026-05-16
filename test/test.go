6851a23f9b3eb2a48e74a0eee604209058887d1a
semenogka <112163326+semenogka@users.noreply.github.com>
2026-05-16 16:36:16 +0500

    in addition to prevoous commit

--- test/test.go
+++ test/test.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 
-	pin "github.com/semenogka/pinterest-downloader"
+	pin "github.com/semenogka/pinterest-downloader/v2"
 )
 
 func main() {
-	err := pin.Client().DownloadFullVideo("https://pin.it/6NgLdOHrQ", "video.mp4", "high", false)
+	err := pin.Client().DownloadFullVideo("some url", "video.mp4", "high", false)
 	if err != nil {
 		log.Println(err)
 	}
